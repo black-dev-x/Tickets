6b72e661e2fc478e6c0dc37e972c285b3c0b78eb
Black <thiago.dev.x@gmail.com>
2021-07-24 09:44:11 -0300

    update

--- Home.js
+++ Home.js
@@ -1,17 +1,41 @@
 import React from 'react'
-import { View, Image, Text } from 'react-native'
+import { View, Image, Text, StyleSheet } from 'react-native'
 
 const Home = props => (
-  <View>
-    <Image source={require('./images/logo-bug.png')}></Image>
-    <Text>Welcome to Black Tickets</Text>
-    <Text>{props.username}</Text>
+  <View style={styles.home}>
+    <Image style={styles.logo} source={require('./images/logo-bug.png')}></Image>
+    <Text style={styles.title}>Welcome to Black Tickets</Text>
+    <Text style={styles.subtitle}>{props.username}</Text>
     <View>
-      <Text>
+      <Text style={styles.text}>
         Id qui eiusmod laboris mollit minim laboris labore amet. Non ullamco labore excepteur cupidatat occaecat nisi incididunt. Et reprehenderit in aute fugiat excepteur irure.
       </Text>
     </View>
   </View>
 )
 
+const styles = StyleSheet.create({
+  home: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: 20
+  },
+  text: {
+    textAlign: 'center',
+    fontFamily: 'Ubuntu-Light',
+    fontWeight: '300',
+    paddingTop: 10
+  },
+  logo: {
+    height: 100,
+    width: 150
+  },
+  title: {
+    fontFamily: 'Ubuntu-Regular'
+  },
+  subtitle: {
+    paddingTop: 5,
+    fontFamily: 'Ubuntu-Regular'
+  }
+})
 export default Home
